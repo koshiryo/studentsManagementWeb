6f78ea523acc712f9a4cc91df909b214923e3242
JiaoziRyo <koshiryo1122@gmail.com>
2019-09-23 22:34:42 +0900

    添加mysql连接工具包 添加AdminDao

---
+++ src/com/students/programmer/dao/AdminDao.java
@@ -0,0 +1,35 @@
+package com.students.programmer.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.students.programmer.model.Admin;
+
+
+
+public class AdminDao extends BaseDao {
+	
+	public Admin login(String name ,String password){
+		String sql = "select * from s_admin where name = '" + name + "' and password = '" + password + "'";
+		ResultSet resultSet = query(sql);
+		try {
+			if(resultSet.next()){
+				Admin admin = new Admin();
+				admin.setId(resultSet.getInt("id"));
+				admin.setName(resultSet.getString("name"));
+				admin.setPassword(resultSet.getString("password"));
+				admin.setStatus(resultSet.getInt("status"));
+				return admin;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public boolean editPassword(Admin admin,String newPassword) {
+		// TODO Auto-generated method stub
+		String sql = "update s_admin set password = '"+newPassword+"' where id = " + admin.getId();
+		return update(sql);
+	}
+}
--- src/com/students/programmer/util/DbUtil.java
+++ src/com/students/programmer/util/DbUtil.java
@@ -1,16 +1,14 @@
 package com.students.programmer.util;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.management.loading.PrivateClassLoader;
 
 public class DbUtil {
 
 	private String dbUrl = "jdbc:mysql://localhost:3306/db_student_manager_web?useUnicode=true&characterEncoding=utf8";
 	private String dbUser = "root";
-	private String dbPassword = "";
+	private String dbPassword = "jiaozi1122";
 	private String jdbcName = "com.mysql.jdbc.Driver";
 	private Connection connection = null;
 	public Connection getConnection(){
@@ -19,7 +17,6 @@ public class DbUtil {
 			connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
 			System.out.println("DB connection succesed");
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			System.out.println("DB connection failed");
 			e.printStackTrace();
 		}
@@ -32,13 +29,11 @@ public class DbUtil {
 				connection.close();
 				System.out.println("DB connection closed");
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		DbUtil dbUtil = new DbUtil();
 		dbUtil.getConnection();
 	}
